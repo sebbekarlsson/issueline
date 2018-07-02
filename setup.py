a7aed98a1aef2058a3691a87ba953d51bd7f9312
Sebastian karlsson <sebbekarlsson97@gmail.com>
2018-07-02 15:06:24 +0200

    removed unused code

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ from setuptools import setup, find_packages
 setup(
     name='issueline',
     version='1.0',
-    install_requires=[
-    ],
     packages=find_packages(),
     entry_points={
         'console_scripts': [
